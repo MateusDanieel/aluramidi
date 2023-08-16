36324ab468e9e87856c2c70ae4d7c65ebc65d8dc
Mateus Daniel <mateus.silva@tritone.com.br>
2023-08-16 14:29:38 -0300

    chore: class 05.02 - Eventos do teclado

--- main.js
+++ main.js
@@ -47,7 +47,11 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
 
 }
 
