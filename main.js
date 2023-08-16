0ef0204b1914687f165adf509f0a3c834788f839
Mateus Daniel <mateus.silva@tritone.com.br>
2023-08-16 11:35:09 -0300

    chore: classes 04.01 to 04.05

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
- function tocaSomPom () {
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+/*
+function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-
 /*
 document.querySelector('.tecla_pom').onclick = tocaSomPom; 
 // AQUI ESTAMOS GUARDANDO A FUNÇÃO DENTRO DO ONCLICK, POR CONTA DISSO NÃO DEVEMOS COLOCAR O '()'
@@ -9,12 +12,38 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom;
 */
 
 // SE O VALOR DA VARIAVEL NUNCA FOR ALTERADO, ELA DEVE SER DECLARADA COMO UMA CONSTANTE (const)
-
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// REFERENCIA VARIAVEL, QUANDO O VALOR DA REFERENCIA PODERÁ SER ALTERADO, ELA DEVE SER DECLARADA COMO (let)
+let contador = 0;
+
+// enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
