d9c174bcfd3204c5d65452f23c408b013ec6d066
Mateus Daniel <mateus.silva@tritone.com.br>
2023-08-16 16:33:25 -0300

    chore: class 05.08 - Melhorando o código

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio).play();
+    // AO CHAMAR O ELEMENTO NO CONSOLE.LOG DO FIREFOX, ELE EXIBE AS PROPRIEDADES DO ELEMENTO, UMA VANTAGEM SOBRE O CHROME
 
-    if (elemento === null) {
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
         //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado');
-    }
-
-    if (elemento != null) {
-        // AO CHAMAR O ELEMENTO NO CONSOLE.LOG DO FIREFOX, ELE EXIBE AS PROPRIEDADES DO ELEMENTO, UMA VANTAGEM SOBRE O CHROME
-        if (elemento.localName === 'audio') {
-            elemento.play();
-        }
-        
+        console.log('Elemento não encontrado ou seletor inválido');
     }
 }
 /*
